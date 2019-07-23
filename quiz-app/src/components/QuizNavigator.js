d6f826330b6a01038e0939e3a49f89d65f7bf2bb
Amin Fazl <amin_fazl@yahoo.com>
2019-07-24 09:04:29 +1000

    Changed the application to support different type of questions

---
+++ quiz-app/src/components/QuizNavigator.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+function QuizNavigator(props) {
+    return (
+        <div className="quiz-container right-aligned">
+            <button
+                type="button"
+                onClick={props.nextHandler}
+                className={classNames(
+                    "button",
+                    {
+                        "button-disabled": props.isDisabled
+                    }
+                )}
+            >Next</button>
+        </div>
+    );
+}
+
+QuizNavigator.propTypes = {
+    isDisabled: PropTypes.bool,
+    nextHandler: PropTypes.func.isRequired
+};
+
+export default QuizNavigator;
